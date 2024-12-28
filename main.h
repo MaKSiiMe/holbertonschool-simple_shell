d493b7796528be5beb450054d31f32bc729f1be4
knarta <charquen10@gmail.com>
2024-12-28 16:02:29 +0100

    Create the library

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void call_interative_mode();
+void call_non_interactive_mode(int argc, char *argv[]);
+
+
+
+
+
+
+
+#endif
