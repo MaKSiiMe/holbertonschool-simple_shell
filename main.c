7183cffeefb6ad9e2f816e88475d7957ef72fd63
root <root@Chacha.>
2025-01-04 22:12:20 +0100

    Modified adapt to new flowchart

--- main.c
+++ main.c
@@ -5,17 +5,22 @@
  *
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
 	int ret = 0;
 
-	if (argc > 1)
+
+	/*disable CTRL+C from here 
+	signal(SIGINT, SIG_IGN);*/
+	
+	/* check if the STDIN of the program is connected to the terminal*/
+	if (isatty(STDIN_FILENO))
 	{
-		call_non_interactive_mode(argc, argv);
+		call_interactive_mode();
 	}
 	else
 	{
-		call_interative_mode();
+		call_non_interactive_mode();
 	}
 
 	return (ret);
