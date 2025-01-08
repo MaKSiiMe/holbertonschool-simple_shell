000131f1644b61bff127e7b5dd57b7f6b7b6f2fd
root <root@Chacha.>
2025-01-08 13:18:33 +0100

    Modified for CTRL+C handling

--- shell.c
+++ shell.c
@@ -10,9 +10,6 @@ int main(void)
 	int ret = 0;
 
 
-	/*disable CTRL+C from here*/
-	signal(SIGINT, SIG_IGN);
-
 	/* check if the STDIN of the program is connected to the terminal*/
 	if (isatty(STDIN_FILENO))
 	{
