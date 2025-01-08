c7654043e8ca35d0fb637884677fcbb76ca6c41a
root <root@Chacha.>
2025-01-08 20:40:43 +0100

    Modified the code

--- shell.c
+++ shell.c
@@ -5,12 +5,13 @@
  * Return: 0 if success execution
  */
 
+
 int main(void)
 {
 	int ret = 0;
 
-
 	/* check if the STDIN of the program is connected to the terminal*/
+
 	if (isatty(STDIN_FILENO))
 	{
 		call_interactive_mode();
@@ -19,6 +20,5 @@ int main(void)
 	{
 		call_non_interactive_mode();
 	}
-
 	return (ret);
 }
