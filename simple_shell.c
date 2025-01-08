df0aa0c85f0615a437a693a7516e34e2c77f4242
root <root@Chacha.>
2025-01-08 20:41:20 +0100

    Modified the code

--- simple_shell.c
+++ simple_shell.c
@@ -8,24 +8,38 @@
 
 int call_non_interactive_mode(void)
 {
-	int ret = 0, nb_args = 0;
+	int ret = 0, nb_args = 0, cmd_num = 0;
 	ssize_t read_chars = 0;
-	char *cmd_line;
-	size_t len_cmd_line;
+	char *cmd_line = NULL;
+	size_t len_cmd_line = 0;
 	char **args = NULL;
 
 	while (read_chars != -1)
 	{
 		read_chars = getline(&cmd_line, &len_cmd_line, stdin);
+		cmd_num++;
 		if (read_chars == EOF)
 		{
 			free(cmd_line);
 			return (0);
 		}
-		nb_args = parse_cmd_line(cmd_line, &args);
+		if (strncmp(cmd_line, "exit", 4) == 0)
+		{
+			ret = shell_exit(cmd_line, cmd_num);
+			exit(ret);
+		}
+		if (strncmp(cmd_line, "env", 3) == 0)
+		{
+			print_env();
+			continue;
+		}
+		if (cmd_line[0] == '\n')
+			continue;
+
+		nb_args = parse_cmd_line(cmd_line, &args, cmd_num);
 
 		if (nb_args >= 2)
-			synchronus_child_execution(args);
+			synchronus_child_execution(args, cmd_num);
 
 		len_cmd_line = 0;
 		free(cmd_line);
@@ -44,7 +58,7 @@ void interruption_handling(int sig)
 	if (sig == 2)
 	{
 		printf("\n");
-		printf("($) ");
+		printf("$ ");
 		fflush(stdout);
 	}
 }
@@ -57,31 +71,33 @@ void interruption_handling(int sig)
 
 int call_interactive_mode(void)
 {
-	int ret = 0, run = 1, nb_args = 0;
+	int ret = 0, run = 1, nb_args = 0, cmd_num = 0;
 	ssize_t read_chars = 0;
 	char *cmd_line = NULL;
 	size_t len_cmd_line = 0;
 	char **args = NULL;
 
 	signal(SIGINT, interruption_handling);
-
 	while (run)
 	{
-		printf("($) ");
+		printf("$ ");
 		fflush(stdin);
 		args = NULL;
 		read_chars = getline(&cmd_line, &len_cmd_line, stdin);
+		cmd_num++;
 		if (read_chars == EOF)
 		{
 			free(cmd_line);
 			run = 0;
 			printf("\n");
-			continue;
+			exit(ret);
 		}
 		len_cmd_line = 0;
 		if (strncmp(cmd_line, "exit", 4) == 0)
-			shell_exit(cmd_line);
-
+		{
+			ret = shell_exit(cmd_line, cmd_num);
+			continue;
+		}
 		if (strncmp(cmd_line, "env", 3) == 0)
 		{
 			print_env();
@@ -90,9 +106,9 @@ int call_interactive_mode(void)
 		if (cmd_line[0] == '\n')
 			continue;
 
-		nb_args = parse_cmd_line(cmd_line, &args);
+		nb_args = parse_cmd_line(cmd_line, &args, cmd_num);
 		if (nb_args >= 2)
-			synchronus_child_execution(args);
+			ret = synchronus_child_execution(args, cmd_num);
 	}
 	return (ret);
 }
