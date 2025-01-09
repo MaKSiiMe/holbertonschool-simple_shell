b56cfc595e9cef975a654b891939450aeac4a925
knarta <charquen10@gmail.com>
2025-01-09 12:24:25 +0100

    Fix memory leak in call_non_interactive_mode

--- execute_command.c
+++ execute_command.c
@@ -8,126 +8,124 @@
  */
 int synchronus_child_execution(char *args[], int cmd_num)
 {
-	int ret = 0, ret_exec = 0, status = 0;
-	pid_t pid;
-	char *tmp = NULL;
-	char **_env = environ;
+    int ret = 0, ret_exec = 0, status = 0;
+    pid_t pid;
+    char *tmp = NULL;
+    char **_env = environ;
 
-	tmp = find_in_path(args[0]);
-	if (!tmp)
-		tmp = args[0];
-	if (access(tmp, F_OK) != -1)
-	{
-		if (access(tmp, X_OK) != -1)
-		{	pid = fork();
-			if (pid < 0)
-			{	print_error_message("Cannot create child process", args[0], cmd_num);
-				return (1);
-			}
-			else if (pid == 0)
-			{
-				ret_exec = execve(tmp, args, _env);
-				exit(ret_exec);
-			}
-			else
-			{
-				wait(&status);
-				if (WIFEXITED(status))
-					ret = WEXITSTATUS(status);
-			}
-		}
-		else
-		{
-			print_error_message("Permission denied", args[0], cmd_num);
-			return (126);
-		}
-	}
-	else
-	{
-		print_error_message("not found", args[0], cmd_num);
-		return (127);
-	}
-	return (ret);
+    tmp = find_in_path(args[0]);
+    if (!tmp)
+        tmp = args[0];
+    if (access(tmp, F_OK) != -1)
+    {
+        if (access(tmp, X_OK) != -1)
+        {
+            pid = fork();
+            if (pid < 0)
+            {
+                print_error_message("Cannot create child process", args[0], cmd_num);
+                return (1);
+            }
+            else if (pid == 0)
+            {
+                ret_exec = execve(tmp, args, _env);
+                exit(ret_exec);
+            }
+            else
+            {
+                wait(&status);
+                if (WIFEXITED(status))
+                    ret = WEXITSTATUS(status);
+            }
+        }
+        else
+        {
+            print_error_message("Permission denied", args[0], cmd_num);
+            if (tmp != args[0])
+                free(tmp);
+            return (126);
+        }
+    }
+    else
+    {
+        print_error_message("not found", args[0], cmd_num);
+        if (tmp != args[0])
+            free(tmp);
+        return (127);
+    }
+    if (tmp != args[0])
+        free(tmp);
+    return (ret);
 }
-/**
- *
- *
- *
- */
 
+/**
+ * my_strdup - function to duplicate a string
+ * @copy_str: string to duplicate
+ * Return: pointer to the duplicated string
+ */
 char *my_strdup(const char *copy_str)
 {
-	char *ret = NULL;
-	size_t len = strlen(copy_str) + 1;
+    char *ret = NULL;
+    size_t len = strlen(copy_str) + 1;
 
-	if (!copy_str)
-		return (NULL);
-	ret = malloc(len);
+    if (!copy_str)
+        return (NULL);
+    ret = malloc(len);
 
-	if (!ret)
-		return (NULL);
-	memcpy(ret, copy_str, len);
-	return (ret);
+    if (!ret)
+        return (NULL);
+    memcpy(ret, copy_str, len);
+    return (ret);
 }
-
 
 /**
  * parse_cmd_line - function parses a command line and transform an array args
- * @ret: pointer to an array that will receive the adress of the array of args
+ * @ret: pointer to an array that will receive the address of the array of args
  * @cmd_line: String containing the command line to be parsed
  * @cmd_num: Command line number
  * Return: the number of arguments
  */
-
 int parse_cmd_line(char *cmd_line, char ***ret, int cmd_num)
 {
-	char *tmp = NULL;
-	int i = 0, size = 0;
-	char *copy = NULL;
-	
-	copy = strdup(cmd_line);
-	
-	printf("%p %s", cmd_line, cmd_line);
+    char *tmp = NULL;
+    int i = 0, size = 0;
+    char *copy = NULL;
 
-	if ((*ret))
-	{
-		print_error_message("Error ret is not NULL", "./hsh", cmd_num);
-		free(copy);
-		return (0);
-	}
-	if (!copy)
-	{
-		print_error_message("Error with strdup", "./hsh", cmd_num);
-		return (0);
-	}
-	tmp = strtok(copy, " ");
-	while (tmp)
-	{
-		size++;
-		tmp = strtok(NULL, " ");
-	}
-	size++;
-	*ret = (char **)malloc(sizeof(char *) * size);
-	if (!(*ret))
-	{
-		free(copy);
-		exit(0);
-	}
-	free(tmp);
-	tmp = strtok(cmd_line, " \n");
-	while (tmp)
-	{
-		(*ret)[i] = my_strdup(tmp);
-		i++;
-		tmp = strtok(NULL, " \n");
-	
-	}
-	(*ret)[i] = NULL;
+    copy = strdup(cmd_line);
 
-/*	for (i = 0; i < size - 1; i++)
-		free((*ret)[i]);*/
-	free(*ret);
-	free(copy);
-	free(tmp);
-	return (size);
+    if ((*ret))
+    {
+        print_error_message("Error ret is not NULL", "./hsh", cmd_num);
+        free(copy);
+        return (0);
+    }
+    if (!copy)
+    {
+        print_error_message("Error with strdup", "./hsh", cmd_num);
+        return (0);
+    }
+    tmp = strtok(copy, " ");
+    while (tmp)
+    {
+        size++;
+        tmp = strtok(NULL, " ");
+    }
+    size++;
+    *ret = (char **)malloc(sizeof(char *) * size);
+    if (!(*ret))
+    {
+        free(copy);
+        exit(0);
+    }
+    tmp = strtok(cmd_line, " \n");
+    while (tmp)
+    {
+        (*ret)[i] = my_strdup(tmp);
+        i++;
+        tmp = strtok(NULL, " \n");
+    }
+    (*ret)[i] = NULL;
+
+    free(copy);
+    return (size);
 }
